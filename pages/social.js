e92f566fee6a47fa31e4a0ae89a2727f125ad029
Tato Marinelli <marinelli.franco1@gmail.com>
2021-10-24 16:05:13 -0300

    Section dev started

---
+++ pages/social.js
@@ -0,0 +1,14 @@
+import { Container, Heading, SimpleGrid, Divider } from '@chakra-ui/react'
+import Section from '../components/section'
+
+const Social = () => {
+    return (
+        <Container>
+                <Heading as="h3" fontSize={20} mb={4} >
+                    Social - Links
+                </Heading>
+        </Container>
+    )
+}
+
+export default Social
