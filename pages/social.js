56b90f2ebd38ad952ac5fdc30d79b00c19838e45
Tato Marinelli <marinelli.franco1@gmail.com>
2021-10-27 19:24:03 -0300

    Static social media

--- pages/social.js
+++ pages/social.js
@@ -15,9 +15,33 @@ import Section from '../components/section'
 
 
 const Social = ({ socialItem, instagramItem }) => {
+
+    /* STATIC */
+    var _socialItem =
+        [
+            { name: "Spotify", url: "https://open.spotify.com/artist/7uXCDw5tJqpxQmqTK4pB1S?si=5I8E58TCTW24ci0F1LbHDw", img: "https://i.imgur.com/0TlOpTk.png" },
+            { name: "Youtube", url: "UC9IvWSGBmu7xG1KpnHgaY3g", img: "https://i.imgur.com/jQEL3cu.png" },
+            { name: "Instagram", url: "https://www.instagram.com/phere_/", img: "https://i.imgur.com/bCoBx63.png" }
+        ]
+
+
+    var _instagramItem =
+        [
+            { url: "https://open.spotify.com/track/7AXE4epsgD37BJI3LCrxFV?si=85374f339e374825&nd=1", img: "https://i.imgur.com/93DQGCt_d.webp?maxwidth=760&fidelity=grand" },
+            { url: "https://www.youtube.com/channel/UC9IvWSGBmu7xG1KpnHgaY3g", img: "https://i.imgur.com/RHYtjhV.jpg" }
+        ]
+
+    if (socialItem == null) {
+        socialItem = _socialItem;
+    }
+    if (instagramItem == null) {
+        instagramItem = _instagramItem;
+    }
+    /*=================================================*/
+
     return (
         <Container maxW="container.xl">
-            
+
             <Section delay={0.1}>
                 <Heading as="h3" fontSize={20} mb={4} paddingTop={5}>
                     Social - Links
@@ -33,8 +57,8 @@ const Social = ({ socialItem, instagramItem }) => {
                             bg="#263238"
                             borderRadius='lg'
                             borderWidth='2px' p='2' marginBottom='2' maxH='-moz-fit-content'
-                            scale={{transition: 'all .15s'}}
-                            _hover={{ bg: "#37474F", transform: 'scale(1.01)', transition: 'all .15s'}}
+                            scale={{ transition: 'all .15s' }}
+                            _hover={{ bg: "#37474F", transform: 'scale(1.01)', transition: 'all .15s' }}
                         >
                             <Image boxSize={7} src={item.img} />
                             <Box lineHeight="tight" ml='2'> {item.name} </Box>
@@ -56,21 +80,20 @@ const Social = ({ socialItem, instagramItem }) => {
             </Section>
 
             <Section delay={0.35}>
-                    <SimpleGrid columns={3} spacing={1} width="full">
-                        {instagramItem.map((item) => (
-                            <Link isExternal href={item.url} key={item.id} color="White">
-                                <GridItem 
-                                    _hover={{ bg: "#37474F", transform: 'scale(1.2)', transition: 'all .15s'}}
-                                    boxShadow="lg"
-                                    maxW="xl" borderWidth="2px" borderColor="whiteAlpha.300" borderRadius="lg" overflow="hidden">
-                                    <Image src={item.img} alt="instagram post" />
-                                </GridItem>
-                            </Link>
-                        ))}
-                    </SimpleGrid>
+                <SimpleGrid columns={3} spacing={1} width="full">
+                    {instagramItem.map((item) => (
+                        <Link isExternal href={item.url} key={item.id} color="White">
+                            <GridItem
+                                _hover={{ bg: "#37474F", transform: 'scale(1.2)', transition: 'all .15s' }}
+                                boxShadow="lg"
+                                maxW="xl" borderWidth="2px" borderColor="whiteAlpha.300" borderRadius="lg" overflow="hidden">
+                                <Image src={item.img} alt="instagram post" />
+                            </GridItem>
+                        </Link>
+                    ))}
+                </SimpleGrid>
             </Section>
         </Container >
-
     )
 }
 
@@ -78,10 +101,17 @@ export default Social
 
 
 export async function getServerSideProps() {
-    const prisma = new PrismaClient();
-    const [socialItem, instagramItem] = await Promise.all([
-        prisma.socialItem.findMany(),
-        prisma.instagramItem.findMany()
-    ]);
-    return { props: { socialItem, instagramItem } };
+    try {
+        const prisma = new PrismaClient();
+        const [socialItem, instagramItem] = await Promise.all([
+            prisma.socialItem.findMany(),
+            prisma.instagramItem.findMany()
+        ]);
+        return { props: { socialItem, instagramItem } };
+    } catch (error) {
+
+        var test = null;
+
+        return { props: { test, test } };
+    }
 }
