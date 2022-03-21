c3895f076c5c8674eea6ad5ff870ad54025834e4
Franco Marinelli <39309862+tatomarinelli@users.noreply.github.com>
2022-03-21 11:27:26 -0300

    Update social.js

--- pages/social.js
+++ pages/social.js
@@ -25,13 +25,13 @@ const Social = ({ _socialItem, _instagramItem }) => {
             { name: "SoundCloud", url: "https://soundcloud.com/phere_ar", img: "https://i.imgur.com/n79HQ3e.png" }
         ]
 
-
+// Ultimo item, primero en renderizar.
     var instagramItem =
         [
-            { url: "https://open.spotify.com/album/4VlWutsOaLf6SRvbUtZiy1?si=SAWjcgizSni666hUXm9TRQ&utm_source=copy-link", img: "https://i.imgur.com/w2flR8E.png" },
+            { url: "https://youtu.be/dLYPlsjGoRo", img: "https://i.imgur.com/w2flR8E.png" },
             { url: "https://open.spotify.com/track/2uRduSbWskSvUNnCgqBM0C?si=77a902b3c1f94844", img: " https://i.imgur.com/mAKQulr.png" },
             { url: "https://open.spotify.com/track/7AXE4epsgD37BJI3LCrxFV?si=85374f339e374825&nd=1", img: "https://i.imgur.com/93DQGCt_d.webp?maxwidth=760&fidelity=grand" },
-            { url: "https://youtu.be/dLYPlsjGoRo", img: "https://i.imgur.com/RHYtjhV.jpg" }
+            { url: "https://www.youtube.com/channel/UC9IvWSGBmu7xG1KpnHgaY3g/featured", img: "https://i.imgur.com/RHYtjhV.jpg" }
         ]
 
     /*
